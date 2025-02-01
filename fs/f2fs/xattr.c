70330ecaf263ad1aceec5217d22f5fa359a63f65
ChioriChioriyaBoutique <chioriofchioriyaboutique@gmail.com>
2025-02-01 01:20:59 +0000

    Revert "UPSTREAM: f2fs: fix deadlock in i_xattr_sem and inode page lock"
    This reverts commit 975d9df96db06b8cd7c19f9f1ec40e04e5e2b3ba.

--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -506,12 +506,10 @@ int f2fs_getxattr(struct inode *inode, int index, const char *name,
 	if (len > F2FS_NAME_LEN)
 		return -ERANGE;
 
-	if (!ipage)
-		down_read(&F2FS_I(inode)->i_xattr_sem);
+	down_read(&F2FS_I(inode)->i_xattr_sem);
 	error = lookup_all_xattrs(inode, ipage, index, len, name,
 				&entry, &base_addr, &base_size);
-	if (!ipage)
-		up_read(&F2FS_I(inode)->i_xattr_sem);
+	up_read(&F2FS_I(inode)->i_xattr_sem);
 	if (error)
 		return error;
 
